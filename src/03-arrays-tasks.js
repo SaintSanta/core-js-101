363e2cb84ae92f53597ded2cf554f56101039134
SaintSanta <aleks2art@gmail.com>
2020-12-14 23:18:36 +0300

    feat: add solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -565,8 +565,9 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const halfArr = Math.floor(arr.length / 2);
+  return arr.slice(-halfArr).concat(arr.slice(halfArr, -halfArr), arr.slice(0, halfArr));
 }
 
 
